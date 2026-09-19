51c54885e26ad4d16f67e8886d353f55d8fabc18
Vitali Baumtrok <vbsw@mailbox.org>
2026-09-19 19:30:16 +0200

    err

---
+++ include/vb/err.h
@@ -0,0 +1,60 @@
+/*
+ *          Copyright 2026, Vitali Baumtrok.
+ * Distributed under the Boost Software License, Version 1.0.
+ *     (See accompanying file LICENSE or copy at
+ *        http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef VBSW_VB_ERR
+#define VBSW_VB_ERR
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+	char *str;
+	int64_t num1;
+	int64_t num2;
+} vb_err_t;
+
+vb_err_t *vb_err_new     (int64_t num1, int64_t num2, const char *str1, const char *str2);
+vb_err_t *vb_err_new_oom (int64_t num1, int64_t num2, const char *str2);
+vb_err_t *vb_err_free    (vb_err_t *err);
+
+#define VB_ERR_NONE                       0
+#define VB_ERR(a)                        (a > 0 && a < 5)
+#define VB_ERR_OUT_OF_MEMORY              1
+#define VB_ERR_STR1_OVERFLOW              2
+#define VB_ERR_STR2_OVERFLOW              3
+#define VB_ERR_NUM2_OVERFLOW              4
+
+#define VB_ERR_MST(a)                    (a > 4 && a < 9)
+#define VB_ERR_MST_UNDERFLOW              5
+#define VB_ERR_MST_OVERFLOW               6
+#define VB_ERR_MST_LIMIT_EXCEEDED         7
+#define VB_ERR_MST_OOM                    8
+
+#define VB_ERR_MHP(a)                    (a > 8 && a < 13)
+#define VB_ERR_MHP_UNDERFLOW              9
+#define VB_ERR_MHP_OVERFLOW              10
+#define VB_ERR_MHP_LIMIT_EXCEEDED        11
+#define VB_ERR_MHP_OOM                   12
+
+#define VB_ERR_MAR(a)                    (a > 12 && a < 17)
+#define VB_ERR_MAR_UNDERFLOW             13
+#define VB_ERR_MAR_OVERFLOW              14
+#define VB_ERR_MAR_LIMIT_EXCEEDED        15
+#define VB_ERR_MAR_OOM                   16
+
+#define VB_ERR_TME(a)                    (a > 16 && a < 19)
+#define VB_ERR_TME_CLOCK                 17
+#define VB_ERR_TME_MONO                  18
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VBSW_VB_ERR */
---
+++ src/utl/err.c
@@ -0,0 +1,113 @@
+/*
+ *          Copyright 2026, Vitali Baumtrok.
+ * Distributed under the Boost Software License, Version 1.0.
+ *     (See accompanying file LICENSE or copy at
+ *        http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vb/err.h>
+
+static vb_err_t oom_err = { "out of memory (1)", VB_ERR_OUT_OF_MEMORY, VB_ERR_NONE };
+static vb_err_t s1o_err = { "str1 size overflow (2)", VB_ERR_STR1_OVERFLOW, VB_ERR_NONE };
+static vb_err_t s2o_err = { "str2 size overflow (3)", VB_ERR_STR2_OVERFLOW, VB_ERR_NONE };
+static vb_err_t n2o_err = { "num2 size overflow (4)", VB_ERR_NUM2_OVERFLOW, VB_ERR_NONE };
+
+vb_err_t *vb_err_new(const int64_t num1, const int64_t num2, const char *const str1, const char *const str2) {
+	assert(num1);
+	vb_err_t *err = NULL;
+	int64_t n1 = num1 > 0 ? num1 : -num1, n2 = num2 > 0 ? num2 : -num2;
+	char n1str[22], n2str[22];
+	size_t n1len = 0, n2len = 0;
+	const size_t str1len = (str1 ? strlen(str1) : 0);
+	const size_t str2len = (str2 ? strlen(str2) : 0);
+	do {
+		n1str[n1len++] = '0' + (n1%10);
+		n1 /= 10;
+	} while (n1 != 0);
+	if (num1 < 0)
+		n1str[n1len++] = '-';
+	if (n2 != 0) {
+		do {
+			n2str[n2len++] = '0' + (n2%10);
+			n2 /= 10;
+		} while (n2 != 0);
+		if (num2 < 0)
+			n2str[n2len++] = '-';
+	}
+	// string pattern: str1 (num1; num2); str2
+	size_t err_size = sizeof(vb_err_t) + n1len + (num2 != 0 ? n2len + 2 : 0) + 3; // 2 = "; ", 3 = "()\0"
+	if (str1len > 0) {
+		if (err_size + 3 <= SIZE_MAX - str1len) {
+			err_size += (str1len + 1); // 1 = " "
+			if (str2len > 0) {
+				if (err_size + 2 <= SIZE_MAX - str1len) {
+					err_size += (str1len + 2); // 2 = "; "
+				} else {
+					err = &s2o_err;
+					err->num2 = num1;
+				}
+			}
+		} else {
+			err = &s1o_err;
+			err->num2 = num1;
+		}
+	} else if (str2len > 0) {
+		if (err_size + 2 <= SIZE_MAX - str1len) {
+			err_size += (str1len + 2); // 2 = "; "
+		} else {
+			err = &s2o_err;
+			err->num2 = num1;
+		}
+	}
+	if (err == NULL) {
+		err = (vb_err_t*)malloc(err_size);
+		if (err) {
+			char *const data = (char*)(&err[1]);
+			size_t offset = 0;
+			if (str1len) {
+				memcpy((void*)data, (const void*)str1, str1len);
+				data[str1len] = ' ';
+				offset = str1len+1;
+			}
+			data[offset++] = '(';
+			for (int i = 0; i < n1len; i++)
+				data[offset+i] = n1str[n1len-1-i];
+			offset += n1len;
+			if (num2) {
+				data[offset++] = ';';
+				data[offset++] = ' ';
+				for (int i = 0; i < n2len; i++)
+					data[offset+i] = n2str[n2len-1-i];
+				offset += n2len;
+			}
+			data[offset++] = ')';
+			if (str2len) {
+				data[offset++] = ';';
+				data[offset++] = ' ';
+				memcpy((void*)&data[offset], (const void*)str2, str2len);
+				offset += str2len;
+			}
+			data[offset] = '\0';
+			err->str = data;
+			err->num1 = num1;
+			err->num2 = num2;
+		} else {
+			err = &oom_err;
+			err->num2 = num1;
+		}
+	}
+	return err;
+}
+
+vb_err_t *vb_err_new_oom(const int64_t num1, const int64_t num2, const char *const str2) {
+	return vb_err_new(num1, num2, "out of memory", str2);
+}
+
+vb_err_t *vb_err_free(vb_err_t *const err) {
+	if (err && err != &oom_err && err != &s1o_err && err != &s2o_err && err != &n2o_err)
+		free((void*)err);
+	return NULL;
+}
